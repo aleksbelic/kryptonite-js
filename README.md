40c3ac83ff2d9815105af7cb2ce91a616481d448
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2022-08-14 02:55:31 +0200

    Adding EN, DE & CR-CYR alphabet, Prettier dep, TS config.

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Kryptonite-TS
 
 TypeScript Crypto Lib.
+
+Ciphers:
+
+- Caesar
+
+## Development
+
+Code formatting is done by [Prettier](https://prettier.io/).
+Simply format project files by following predefined rules in `.prettierrc.json`:
+
+```
+$ npm run pretty
+```
+
+which is an alias for:
+
+```
+$ npx prettier --write .
+```
+
+To exclude files from formatting, please refer to `.prettierignore` (corresponds to `.gitignore`).
---
+++ src/caesar.ts
@@ -0,0 +1,18 @@
+import {ALPHABET_EN} from './helper';
+
+/**
+ * Caesar cipher.
+ * @param plaintext text to be encrypted
+ * @param shift number of left or right alphabet rotations
+ * @param alphabet alphabet being used
+ * @returns ciphertext
+ * @author Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
+ */
+export const encrypt = (
+  plaintext: string,
+  shift: number,
+  alphabet = ALPHABET_EN
+): string => {
+  const shiftedAlphabet = [];
+  return 'Caesar';
+};
