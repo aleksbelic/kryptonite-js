394aae802608df135b374f2ef7fb6328cbabf378
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2022-09-15 01:29:58 +0200

    Fixes Vigenere cipher and implements case-sensitivity; new tests.

--- src/ciphers/vigenere.ts
+++ src/ciphers/vigenere.ts
@@ -1,4 +1,5 @@
 import {ALPHABET_EN} from '../globals.js';
+import {isUpperCase} from '../helpers.js';
 
 /**
  * [Vigenère cipher](https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher) encryption.
@@ -10,7 +11,12 @@ import {ALPHABET_EN} from '../globals.js';
  * @returns ciphertext
  * @author Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
  * @example
- * TODO
+ * encrypt('My secret msg', 'mykey')
+ * returns 'Yw ciadcd qqs'
+ * encrypt('My secret msg', 'mykey', false)
+ * returns 'yw ciadcd qqs'
+ * encrypt('My secret msg', 'mykey', undefined, false)
+ * returns 'Ywciadcdqqs'
  */
 export function encrypt(
   plaintext: string,
@@ -24,22 +30,28 @@ export function encrypt(
   }
 
   let ciphertext = '',
-    currentChar: string;
+    currentChar: string,
+    currentCharEncrypted: string;
 
-  for (let i = 0; i < plaintext.length; i++) {
+  for (let i = 0, j = 0; i < plaintext.length; i++, j++) {
     currentChar = plaintext[i];
-    if (alphabet.indexOf(currentChar) === -1) {
+    if (alphabet.indexOf(currentChar.toLowerCase()) === -1) {
       if (includeForeignChars) {
         ciphertext += currentChar;
       }
+      --j;
       continue;
     }
-    ciphertext +=
+    currentCharEncrypted =
       alphabet[
-        (alphabet.indexOf(currentChar) +
-          alphabet.indexOf(key[i % key.length])) %
+        (alphabet.indexOf(currentChar.toLowerCase()) +
+          alphabet.indexOf(key[j % key.length])) %
           alphabet.length
       ];
+    ciphertext +=
+      caseSensitive && isUpperCase(currentChar)
+        ? currentCharEncrypted.toUpperCase()
+        : currentCharEncrypted;
   }
   return ciphertext;
 }
@@ -48,30 +60,53 @@ export function encrypt(
  * [Vigenère cipher](https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher) decryption.
  * @param ciphertext text to be decrypted
  * @param key word used to switch cipher alphabets every letter
+ * @param [caseSensitive=true] if correct input of upper case and lower case matters
+ * @param [includeForeignChars=true] if unknown char should be included in plaintext
  * @param alphabet used alphabet
  * @returns plaintext
  * @author Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
  * @example
- * TODO
+ * decrypt('Yw ciadcd qqs', 'mykey')
+ * returns 'My secret msg'
+ * decrypt('Yw ciadcd qqs', 'mykey', false)
+ * returns 'my secret msg'
+ * decrypt('Yw ciadcd qqs', 'mykey', undefined, false)
+ * returns 'Mysecretmsg'
  */
 export function decrypt(
   ciphertext: string,
   key: string,
+  caseSensitive = true,
+  includeForeignChars = true,
   alphabet = ALPHABET_EN
 ): string {
   if (key === '') {
     throw Error('No key provided.');
   }
 
-  let plaintext = '';
-  let currentDecryptedCharIndex: number;
-  for (let i = 0; i < ciphertext.length; i++) {
-    currentDecryptedCharIndex =
-      alphabet.indexOf(ciphertext[i]) - alphabet.indexOf(key[i % key.length]);
+  let plaintext = '',
+    currentChar: string,
+    currentCharDecryptedIndex: number,
+    currentCharDecrypted: string;
+
+  for (let i = 0, j = 0; i < ciphertext.length; i++, j++) {
+    currentChar = ciphertext[i];
+    if (alphabet.indexOf(currentChar.toLowerCase()) === -1) {
+      if (includeForeignChars) {
+        plaintext += currentChar;
+      }
+      --j;
+      continue;
+    }
+    currentCharDecryptedIndex =
+      alphabet.indexOf(currentChar.toLowerCase()) -
+      alphabet.indexOf(key[j % key.length]);
+    currentCharDecrypted =
+      alphabet[(currentCharDecryptedIndex + alphabet.length) % alphabet.length];
     plaintext +=
-      alphabet[
-        (currentDecryptedCharIndex += alphabet.length) % alphabet.length
-      ];
+      caseSensitive && isUpperCase(currentChar)
+        ? currentCharDecrypted.toUpperCase()
+        : currentCharDecrypted;
   }
   return plaintext;
 }
--- tests/ciphers/vigenere.test.ts
+++ tests/ciphers/vigenere.test.ts
@@ -2,22 +2,42 @@ import {encrypt, decrypt} from '../../src/ciphers/vigenere.js';
 
 describe('Vigenère cipher - encryption', () => {
   test('Basic', () => {
+    expect(encrypt('', 'secretkey')).toEqual('');
     expect(encrypt('abcdefghijklmnopqrstuvwxyz', 'cowabunga')).toEqual(
       'cpydfztnilyhmoicwruhqvxrlf'
     );
     expect(encrypt('this is my secret message', 'secretkey')).toEqual(
       'llkj ml wc qwgtvx fowqskg'
     );
+    expect(encrypt('My name is Bond, James Bond.', 'secretkey', true)).toEqual(
+      'Ec prqx sw Zgrf, Aefow Zgrf.'
+    );
+    expect(encrypt('My name is Bond, James Bond.', 'secretkey', false)).toEqual(
+      'ec prqx sw zgrf, aefow zgrf.'
+    );
+    expect(
+      encrypt('My name is Bond, James Bond.', 'secretkey', undefined, false)
+    ).toEqual('EcprqxswZgrfAefowZgrf');
   });
 });
 
 describe('Vigenère cipher - decryption', () => {
   test('Basic', () => {
+    expect(decrypt('', 'secretkey')).toEqual('');
     expect(decrypt('cpydfztnilyhmoicwruhqvxrlf', 'cowabunga')).toEqual(
       'abcdefghijklmnopqrstuvwxyz'
     );
-    expect(encrypt('llkj ml wc qwgtvx fowqskg', 'secretkey')).toEqual(
+    expect(decrypt('llkj ml wc qwgtvx fowqskg', 'secretkey')).toEqual(
       'this is my secret message'
     );
+    expect(decrypt('Ec prqx sw Zgrf, Aefow Zgrf.', 'secretkey', true)).toEqual(
+      'My name is Bond, James Bond.'
+    );
+    expect(decrypt('Ec prqx sw Zgrf, Aefow Zgrf.', 'secretkey', false)).toEqual(
+      'my name is bond, james bond.'
+    );
+    expect(
+      decrypt('Ec prqx sw Zgrf, Aefow Zgrf.', 'secretkey', undefined, false)
+    ).toEqual('MynameisBondJamesBond');
   });
 });
