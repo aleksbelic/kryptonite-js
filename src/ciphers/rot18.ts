29475c3b1c7aba18d55dbc58d357cfd8f0495348
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2022-09-14 11:14:42 +0200

    Adding decrypt function for ROT18 cipher.

--- src/ciphers/rot18.ts
+++ src/ciphers/rot18.ts
@@ -42,3 +42,28 @@ export function encrypt(
   } else ciphertext = tmpCiphertext;
   return ciphertext;
 }
+
+/**
+ * Variant of [ROT13 cipher](https://en.wikipedia.org/wiki/ROT13) decryption that applies to both digits and letters.
+ * @param ciphertext text to be decrypted
+ * @param [caseSensitive=true] if correct input of upper case and lower case matters
+ * @param [includeForeignChars=true] if unknown char should be included in plaintext
+ * @returns plaintext
+ * @author Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
+ * @example
+ * decrypt('678nOp')
+ * // returns '123aBc'
+ * decrypt('678nOp', false)
+ * // returns '123abc'
+ * decrypt('678!nOp', undefined, true)
+ * // returns '123!aBc'
+ * decrypt('678!nOp', undefined, false)
+ * // returns '123aBc'
+ */
+export function decrypt(
+  ciphertext: string,
+  caseSensitive = true,
+  includeForeignChars = true
+): string {
+  return encrypt(ciphertext, caseSensitive, includeForeignChars);
+}
--- tests/ciphers/rot18.test.ts
+++ tests/ciphers/rot18.test.ts
@@ -1,13 +1,25 @@
-import {encrypt} from '../../src/ciphers/rot18.js';
+import {encrypt, decrypt} from '../../src/ciphers/rot18.js';
 
 describe('ROT18 cipher - encryption', () => {
   test('Shift', () => {
     expect(encrypt('1234567890abcdefghijklmnopqrstuvwxyz')).toEqual(
       '6789012345nopqrstuvwxyzabcdefghijklm'
     );
-    expect(encrypt('123aBc', true)).toEqual('678nOp');
+    expect(encrypt('123aBc')).toEqual('678nOp');
     expect(encrypt('123aBc', false)).toEqual('678nop');
     expect(encrypt('123 aBc', undefined, true)).toEqual('678 nOp');
     expect(encrypt('123 aBc', undefined, false)).toEqual('678nOp');
+  });
+});
+
+describe('ROT18 cipher - decryption', () => {
+  test('Shift', () => {
+    expect(decrypt('6789012345nopqrstuvwxyzabcdefghijklm')).toEqual(
+      '1234567890abcdefghijklmnopqrstuvwxyz'
+    );
+    expect(decrypt('678nOp')).toEqual('123aBc');
+    expect(decrypt('678nOp', false)).toEqual('123abc');
+    expect(decrypt('678!nOp', undefined, true)).toEqual('123!aBc');
+    expect(decrypt('678!nOp', undefined, false)).toEqual('123aBc');
   });
 });
