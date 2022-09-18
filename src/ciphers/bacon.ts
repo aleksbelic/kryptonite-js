ac0a3cc34358f6ae5b7f6c56c089b13cd424af4b
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2022-09-19 00:35:02 +0200

    Adding Bacon's cipher decryption with tests for both versions.

--- src/ciphers/bacon.ts
+++ src/ciphers/bacon.ts
@@ -1,4 +1,5 @@
 import {bacon1Map, bacon2Map} from '../globals.js';
+import {getMapKeyByValue} from '../helpers.js';
 
 /**
  * [Bacon's cipher](https://en.wikipedia.org/wiki/Bacon%27s_cipher) encryption.
@@ -24,9 +25,7 @@ export function encrypt(
   version = 2,
   includeForeignChars = true
 ): string {
-  if ([1, 2].indexOf(version) === -1) {
-    throw Error('Bacon cipher version unknown - please select verson 1 or 2.');
-  }
+  checkVersion(version);
   const baconMap = version === 1 ? bacon1Map : bacon2Map;
 
   let ciphertext = '',
@@ -44,4 +43,64 @@ export function encrypt(
   }
 
   return ciphertext;
+}
+
+/**
+ * [Bacon's cipher](https://en.wikipedia.org/wiki/Bacon%27s_cipher) decryption.
+ * @param ciphertext text to be decrypted
+ * @param [version=2] cipher version (1 or 2). 1st version uses the same code for letters 'i' & 'j' as well as for 'u' & v, while the 2nd version has unique code for each letter.
+ * @returns plaintext
+ * @author Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
+ * @example
+ * decrypt('aaaaaaaaabaaaba')
+ * returns 'abc'
+ * decrypt('abaaaabaaabaabbbaabb', 1)
+ * returns 'iiuu'
+ * decrypt('abaaaabaabbabaababab', 2)
+ * returns 'ijuv'
+ * decrypt('aaaaa_aaaab !!! aaaba!')
+ * returns 'abc'
+ */
+export function decrypt(ciphertext: string, version = 2) {
+  checkVersion(version);
+  const baconMap = version === 1 ? bacon1Map : bacon2Map;
+
+  let plaintext = '',
+    fiveBaconChars = '',
+    currentCharDecrypted: string | undefined;
+
+  ciphertext = ciphertext.toLowerCase().replace(/[^ab]/g, '');
+  for (const char of ciphertext) {
+    if (fiveBaconChars.length < 5) {
+      fiveBaconChars += char;
+      if (fiveBaconChars.length === 5) {
+        currentCharDecrypted = getMapKeyByValue(baconMap, fiveBaconChars);
+        if (currentCharDecrypted !== undefined) {
+          plaintext += currentCharDecrypted;
+        }
+        fiveBaconChars = '';
+      }
+    }
+  }
+
+  return plaintext;
+}
+
+export function encryptInRandomText(message: string) {
+  //TODO
+}
+
+/**
+ * Checks if provided Bacon's cipher verion is valid.
+ * @param version cipher version
+ * @returns true if provided version is valid, throws error if invalid
+ * @author Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
+ */
+function checkVersion(version: number): boolean | never {
+  if ([1, 2].indexOf(version) === -1) {
+    throw Error(
+      `Bacon cipher version '${version}' unknown - please select verson 1 or 2.`
+    );
+  }
+  return true;
 }
--- tests/bacon.test.ts
+++ tests/bacon.test.ts
@@ -1,7 +1,7 @@
-import {encrypt} from '../src/ciphers/bacon.js';
+import {encrypt, decrypt} from '../src/ciphers/bacon.js';
 
-describe('Bacon cipher v1 - encryption', () => {
-  test('Basic', () => {
+describe('Bacon cipher v1', () => {
+  test('Encryption', () => {
     expect(encrypt('abcdefghijklmnopqrstuvwxyz', 1)).toEqual(
       'aaaaaaaaabaaabaaaabbaabaaaababaabbaaabbbabaaaabaaaabaabababaababbabbaaabbababbbaabbbbbaaaabaaabbaababaabbbaabbbabaabababbabbababbb'
     );
@@ -11,16 +11,79 @@ describe('Bacon cipher v1 - encryption', () => {
     expect(encrypt('abc jinx vulture', 1, false)).toEqual(
       'aaaaaaaaabaaabaabaaaabaaaabbaabababbaabbbaabbabababaababaabbbaaaaaabaa'
     );
-    expect(() => encrypt('abc', 3)).toThrowError(
-      'Bacon cipher version unknown - please select verson 1 or 2.'
+  });
+
+  test('Decryption', () => {
+    expect(
+      decrypt(
+        'aaaaaaaaabaaabaaaabbaabaaaababaabbaaabbbabaaaabaaaabaabababaababbabbaaabbababbbaabbbbbaaaabaaabbaababaabbbaabbbabaabababbabbababbb',
+        1
+      )
+    ).toEqual('abcdefghiiklmnopqrstuuwxyz');
+    expect(
+      decrypt(
+        'aaaaaaaaabaaaba abaaaabaaaabbaababab baabbbaabbabababaababaabbbaaaaaabaa',
+        1
+      )
+    ).toEqual('abciinxuulture');
+    expect(
+      decrypt(
+        'gFaX8_Akl2:aog4Acz$B6olaeD)Awqµ#aK.OrnaSAJ_ax;kPa*q]_aB=jtn?Aöw€. MdairhGlkBC"=hzBVaÜv%iTmebxaO7hl3bnID+Fb6wkL(a§ö;a_s#7iA|pZbgp2~asoTaöZ8vaqaK/n4rBUep=a03_a!EdlvA71a,3BürbqsxA gA3_lu(1pevB+anh4TB#a6jrEK_9l?öB9€_bUqAv.a=öbBM)R?e55[B1&Äa7AB_9rb6a91A#fBa3d_Ak',
+        1
+      )
+    ).toEqual('baconiinxuue');
+  });
+});
+
+describe('Bacon cipher v2', () => {
+  test('Encryption', () => {
+    // version 2 is default
+    expect(encrypt('abcdefghijklmnopqrstuvwxyz')).toEqual(
+      'aaaaaaaaabaaabaaaabbaabaaaababaabbaaabbbabaaaabaabababaababbabbaaabbababbbaabbbbbaaaabaaabbaababaabbbabaabababbabbababbbbbaaabbaab'
+    );
+    expect(encrypt('abcdefghijklmnopqrstuvwxyz', 2)).toEqual(
+      'aaaaaaaaabaaabaaaabbaabaaaababaabbaaabbbabaaaabaabababaababbabbaaabbababbbaabbbbbaaaabaaabbaababaabbbabaabababbabbababbbbbaaabbaab'
+    );
+    expect(encrypt('abc jinx vulture', 2)).toEqual(
+      'aaaaaaaaabaaaba abaababaaaabbabbabbb bababbabaaababbbaabbbabaabaaabaabaa'
+    );
+    expect(encrypt('abc jinx vulture', 2, false)).toEqual(
+      'aaaaaaaaabaaabaabaababaaaabbabbabbbbababbabaaababbbaabbbabaabaaabaabaa'
     );
   });
 
-  test('Various', () => {
-    expect(encrypt('ijuv')).toEqual('abaaaabaabbabaababab');
-    expect(encrypt('ijuv', 1)).toEqual('abaaaabaaabaabbbaabb');
-    expect(encrypt('ijuv', 2)).toEqual('abaaaabaabbabaababab');
-    expect(encrypt('Abc!', undefined, true)).toEqual('aaaaaaaaabaaaba!');
-    expect(encrypt('Abc!', undefined, false)).toEqual('aaaaaaaaabaaaba');
+  test('Decryption', () => {
+    // version 2 is default
+    expect(
+      decrypt(
+        'aaaaaaaaabaaabaaaabbaabaaaababaabbaaabbbabaaaabaabababaababbabbaaabbababbbaabbbbbaaaabaaabbaababaabbbabaabababbabbababbbbbaaabbaab'
+      )
+    ).toEqual('abcdefghijklmnopqrstuvwxyz');
+    expect(
+      decrypt(
+        'aaaaaaaaabaaabaaaabbaabaaaababaabbaaabbbabaaaabaabababaababbabbaaabbababbbaabbbbbaaaabaaabbaababaabbbabaabababbabbababbbbbaaabbaab',
+        2
+      )
+    ).toEqual('abcdefghijklmnopqrstuvwxyz');
+    expect(
+      decrypt(
+        'aaaaaaaaabaaaba abaababaaaabbabbabbb bababbabaaababbbaabbbabaabaaabaabaa',
+        2
+      )
+    ).toEqual('abcjinxvulture');
+    expect(
+      decrypt(
+        'maD&aFA=a9b5K1ä:a-6rah;AyqvaoK*_a(g4dZuaalAÖbaabMeBbaG3abUBazbaQ8bAP=aQbafbWaA7o]kccanDabKµ923bk#FAbbd_ablBqb71fGb90aBa9oa8bAkb2aBp3aaBf4a6a7hc2jz62ipfr',
+        2
+      )
+    ).toEqual('baconjinxuve');
   });
+});
+
+test('Various', () => {
+  expect(encrypt('Abc!', undefined, true)).toEqual('aaaaaaaaabaaaba!');
+  expect(encrypt('Abc!', undefined, false)).toEqual('aaaaaaaaabaaaba');
+  expect(() => encrypt('abc', 3)).toThrowError(
+    "Bacon cipher version '3' unknown - please select verson 1 or 2."
+  );
 });
