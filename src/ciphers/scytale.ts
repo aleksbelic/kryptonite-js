309c1d5e1cb5e81d3adfb27e3ee4fa8aeae7c84f
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2022-09-25 01:42:44 +0200

    Fixes Scytale cipher encryption; adding Scytale decription with tests.

--- src/ciphers/scytale.ts
+++ src/ciphers/scytale.ts
@@ -1,32 +1,51 @@
 /**
  * [Scytale cipher](https://en.wikipedia.org/wiki/Scytale) encryption.
  * @param plaintext text to be encrypted
- * @param [columnCount=1] number of text columns created by wounding it around a scytale
+ * @param [columnCount=4] number of text columns created by wounding plaintext around a scytale
  * @returns ciphertext
  * @author Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
  * @example
  * encrypt('abcdef', 2)
  * // returns 'acebdf'
  * encrypt('abcdef', 5)
- * // returns 'afbcde'
+ * // returns 'afb c d e'
  */
-export function encrypt(plaintext: string, columnCount = 1): string {
+export function encrypt(plaintext: string, columnCount = 4): string {
   if (columnCount < 1) {
     throw Error('Invalid param: number of columns must be a positive integer.');
   }
-  let ciphertext = '';
-  const plaintextArray = Array.from(plaintext);
 
-  let i = 0;
-  while (ciphertext.length !== plaintext.length) {
-    if (plaintextArray[i] !== '') {
-      ciphertext += plaintextArray[i];
-      plaintextArray[i] = '';
-      i = i + columnCount < plaintext.length ? i + columnCount : 0;
-    } else {
-      i++;
-    }
+  let ciphertextArray: string[] = new Array(columnCount).fill('');
+  const rowCount = Math.ceil(plaintext.length / columnCount);
+
+  Array.from(plaintext).forEach((char, index) => {
+    ciphertextArray[index % columnCount] += char;
+  });
+  ciphertextArray = ciphertextArray.map(cipherTextSubstring =>
+    cipherTextSubstring.padEnd(rowCount)
+  );
+
+  return ciphertextArray.join('').trim();
+}
+
+/**
+ * [Scytale cipher](https://en.wikipedia.org/wiki/Scytale) encryption.
+ * @param ciphertext text to be decrypted
+ * @param columnCount [columnCount=4] number of text columns created by wounding ciphertext around a scytale
+ * @returns plaintext
+ * @author Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
+ * @example
+ * decrypt('acebdf', 2)
+ * // returns 'abcdef'
+ * decrypt('afb c d e', 5)
+ * // returns 'abcdef'
+ */
+export function decrypt(ciphertext: string, columnCount = 4): string {
+  if (columnCount < 1) {
+    throw Error('Invalid param: number of columns must be a positive integer.');
+  } else if (ciphertext.length === 0) {
+    throw Error('Invalid param: no ciphertext provided.');
   }
-
-  return ciphertext;
+  const rowCount = Math.ceil(ciphertext.length / columnCount);
+  return encrypt(ciphertext, rowCount);
 }
--- tests/ciphers/scytale.test.ts
+++ tests/ciphers/scytale.test.ts
@@ -1,27 +1,54 @@
-import {encrypt} from '../../src/ciphers/scytale.js';
+import {encrypt, decrypt} from '../../src/ciphers/scytale.js';
 import {ALPHABET_EN} from '../../src/globals.js';
 
 describe('Scytale cipher', () => {
   test('Encryption', () => {
     expect(encrypt('', 1)).toEqual('');
+    expect(encrypt('abcdef')).toEqual('aebfc d');
     expect(encrypt('abcdef', 1)).toEqual('abcdef');
     expect(encrypt('abcdef', 2)).toEqual('acebdf');
     expect(encrypt('abcdef', 3)).toEqual('adbecf');
-    expect(encrypt('abcdef', 4)).toEqual('aebfcd');
-    expect(encrypt('abcdef', 5)).toEqual('afbcde');
+    expect(encrypt('abcdef', 4)).toEqual('aebfc d');
+    expect(encrypt('abcdef', 5)).toEqual('afb c d e');
     expect(encrypt('abcdef', 6)).toEqual('abcdef');
     expect(encrypt('abcdef', 7)).toEqual('abcdef');
     expect(encrypt([...ALPHABET_EN].join(''), 1)).toEqual(
       [...ALPHABET_EN].join('')
     );
     expect(encrypt([...ALPHABET_EN].join(''), 4)).toEqual(
-      'aeimquybfjnrvzcgkoswdhlptx'
+      'aeimquybfjnrvzcgkosw dhlptx'
     );
     expect(encrypt([...ALPHABET_EN].join(''), 5)).toEqual(
-      'afkpuzbglqvchmrwdinsxejoty'
+      'afkpuzbglqv chmrw dinsx ejoty'
     );
     expect(() => encrypt('abcdef', 0)).toThrowError(
       'Invalid param: number of columns must be a positive integer.'
+    );
+  });
+
+  test('Decryption', () => {
+    expect(decrypt('aebfc d')).toEqual('abcdef');
+    expect(decrypt('abcdef', 1)).toEqual('abcdef');
+    expect(decrypt('acebdf', 2)).toEqual('abcdef');
+    expect(decrypt('adbecf', 3)).toEqual('abcdef');
+    expect(decrypt('aebfc d', 4)).toEqual('abcdef');
+    expect(decrypt('afb c d e', 5)).toEqual('abcdef');
+    expect(decrypt('abcdef', 6)).toEqual('abcdef');
+    expect(decrypt('abcdef', 7)).toEqual('abcdef');
+    expect(decrypt([...ALPHABET_EN].join(''), 1)).toEqual(
+      [...ALPHABET_EN].join('')
+    );
+    expect(decrypt('aeimquybfjnrvzcgkosw dhlptx', 4)).toEqual(
+      [...ALPHABET_EN].join('')
+    );
+    expect(decrypt('afkpuzbglqv chmrw dinsx ejoty', 5)).toEqual(
+      [...ALPHABET_EN].join('')
+    );
+    expect(() => decrypt('abcdef', 0)).toThrowError(
+      'Invalid param: number of columns must be a positive integer.'
+    );
+    expect(() => decrypt('', 1)).toThrowError(
+      'Invalid param: no ciphertext provided.'
     );
   });
 });
